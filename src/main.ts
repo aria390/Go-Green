3e6dc9e8064d8a00066c01044965ced7b08009ee
aria_bz <ariabahmanzangi@gmail.com>
2025-10-19 19:05:09 +0330

    v2

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,37 @@
-import './style.css'
+import "./style.css";
 
+const sliderScreenEl =
+  document.querySelectorAll<HTMLDivElement>(".sliderScreen");
+const leftBtnEl = document.getElementById("leftBtn") as HTMLImageElement;
+const rightBtnEl = document.getElementById("rightBtn") as HTMLImageElement;
+
+rightBtnEl?.addEventListener("click", () => {
+  const currentEl = document.querySelector(".current");
+  currentEl?.classList.remove("current");
+  if (currentEl?.nextElementSibling) {
+    currentEl.nextElementSibling.classList.add("current");
+  } else {
+    sliderScreenEl[0].classList.add("current");
+  }
+  console.log(currentEl);
+});
+
+leftBtnEl?.addEventListener("click", () => {
+  const currentEl = document.querySelector(".current");
+  currentEl?.classList.remove("current");
+  if (currentEl?.previousElementSibling) {
+    currentEl.previousElementSibling.classList.add("current");
+  } else {
+    sliderScreenEl[sliderScreenEl.length - 1].classList.add("current");
+  }
+});
+
+setInterval(() => {
+  const currentEl = document.querySelector(".current");
+  currentEl?.classList.remove("current");
+  if (currentEl?.nextElementSibling) {
+    currentEl.nextElementSibling.classList.add("current");
+  } else {
+    sliderScreenEl[0].classList.add("current");
+  }
+},5000);
